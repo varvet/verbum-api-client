f0422669d3a6ed6e028912157bf6e0e774d7f7c6
Jens Ljungblad <jens.ljungblad@gmail.com>
2015-03-12 10:38:43 +0100

    Adds composers and lyricists to psalms

--- lib/verbum/api/psalm.rb
+++ lib/verbum/api/psalm.rb
@@ -3,6 +3,14 @@ module Verbum
     class Psalm < Base
       attributes :title, :number, :title_with_number, :addition, :note_sheet
       associations :authors, :authorships, :tags, :theme, :verses
+
+      def composers
+        @composers ||= authorships.group_by(&:context)["melody"] || []
+      end
+
+      def lyricists
+        @lyricists ||= authorships.group_by(&:context)["lyrics"] || []
+      end
     end
   end
 end
