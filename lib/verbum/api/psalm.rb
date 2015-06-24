edd39b3d6425c8d5d9f44926c55452f16eef7ec9
Johan Halse <johan.halse@varvet.com>
2015-06-24 15:48:16 +0200

    Add soundcloud stream url to psalms

--- lib/verbum/api/psalm.rb
+++ lib/verbum/api/psalm.rb
@@ -1,7 +1,7 @@
 module Verbum
   module Api
     class Psalm < Base
-      attributes :title, :number, :title_with_number, :addition, :note_sheet
+      attributes :title, :number, :title_with_number, :addition, :note_sheet, :soundcloud_stream_url
       associations :authors, :authorships, :tags, :theme, :verses
 
       def composers
